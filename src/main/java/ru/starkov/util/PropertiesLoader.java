b6be18642d5565a3fbbecf048c85a8bfd304c6dc
StarkovAleksandr <stanley59rus@gmail.com>
2024-03-01 16:58:09 +0500

    Refactor PropertiesLoader for clarity and encapsulation
    Updated PropertiesLoader to enhance clarity and encapsulation.
    Marked the class as final to prevent subclassing and private constructor to prevent instantiation.
    Improved Javadoc comments for documentation and clarity.
    Made minor formatting adjustments for consistency and readability.

--- src/main/java/ru/starkov/util/PropertiesLoader.java
+++ src/main/java/ru/starkov/util/PropertiesLoader.java
@@ -2,27 +2,38 @@ package ru.starkov.util;
 
 import java.io.IOException;
 import java.util.Properties;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-public class PropertiesLoader {
+/**
+ * The PropertiesLoader class is responsible for loading properties from the application.properties
+ * file. It provides a method to retrieve a property value by its key.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PropertiesLoader {
 
-    private final static Properties PROPERTIES = new Properties();
+  private static final Properties PROPERTIES = new Properties();
 
-    static {
-        loadProperties();
+  static {
+    loadProperties();
+  }
+
+  /**
+   * Retrieves the value of the property associated with the specified key.
+   *
+   * @param key the key of the property
+   * @return the value of the property, or null if the key is not found
+   */
+  public static String get(String key) {
+    return PROPERTIES.getProperty(key);
+  }
+
+  private static void loadProperties() {
+    try (var propertiesStream = PropertiesLoader.class.getClassLoader()
+        .getResourceAsStream("application.properties")) {
+      PROPERTIES.load(propertiesStream);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-
-    private PropertiesLoader() {
-    }
-
-    public static String get(String key) {
-        return PROPERTIES.getProperty(key);
-    }
-
-    private static void loadProperties() {
-        try (var propertiesStream = PropertiesLoader.class.getClassLoader().getResourceAsStream("application.properties")) {
-            PROPERTIES.load(propertiesStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 }
