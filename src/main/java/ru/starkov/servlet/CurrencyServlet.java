a1dca9359f746525b962a87d58909b66ba07615c
StarkovAleksandr <stanley59rus@gmail.com>
2024-03-01 16:37:10 +0500

    Refactored CurrencyServlet to handle operations related to a single currency.
    mported necessary constants and classes.
    Annotated the servlet with @WebServlet to specify the URL pattern.
    Overridden the init method to initialize dependencies.
    Implemented the doGet method to handle GET requests.
    Implemented the handleSingleCurrencyRequest method to handle requests related to a single currency.
    Added exception handling for CurrencyNotFoundException and DatabaseException.
    Used the CurrencyMapper to map the currency entity to its DTO representation.
    Used the CurrencyService to retrieve the currency information.
    These changes enhance the CurrencyServlet to effectively manage operations related to a single currency.

--- src/main/java/ru/starkov/servlet/CurrencyServlet.java
+++ src/main/java/ru/starkov/servlet/CurrencyServlet.java
@@ -1,91 +1,67 @@
 package ru.starkov.servlet;
 
-import com.google.gson.Gson;
+import static ru.starkov.util.Constants.CODE;
+import static ru.starkov.util.Constants.CURRENCY_PATH;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ru.starkov.model.Currency;
-import ru.starkov.service.CurrencyService;
-import ru.starkov.servlet.dto.CurrencyDto;
-import ru.starkov.servlet.mapper.CurrencyMapper;
-
 import java.io.IOException;
-import java.util.Optional;
+import ru.starkov.dto.mapper.CurrencyMapper;
+import ru.starkov.exception.CurrencyNotFoundException;
+import ru.starkov.exception.DatabaseException;
+import ru.starkov.service.CurrencyService;
 
-@WebServlet("/currencies/*")
-public class CurrencyServlet extends HttpServlet {
 
-    private static final String CURRENCIES_PATH = "currencies";
+/**
+ * Servlet to manage operations related to a single currency. Extends {@link AbstractHttpServlet}.
+ */
+@WebServlet(
+    name = "CurrencyServlet",
+    description = "Handle operations related to a single currency",
+    value = CURRENCY_PATH + "/*"
+)
+public final class CurrencyServlet extends AbstractHttpServlet {
 
-    private CurrencyService currencyService;
-    private CurrencyMapper currencyMapper;
-    private Gson gson;
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        var servletContext = config.getServletContext();
-        this.currencyService = (CurrencyService) servletContext.getAttribute("currencyService");
-        this.currencyMapper = (CurrencyMapper) servletContext.getAttribute("currencyMapper");
-        this.gson = (Gson) servletContext.getAttribute("gson");
+  private CurrencyService currencyService;
+  private CurrencyMapper currencyMapper;
+
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    var servletContext = config.getServletContext();
+    this.currencyService = (CurrencyService) servletContext.getAttribute(
+        CurrencyService.class.getName());
+    this.currencyMapper = (CurrencyMapper) servletContext.getAttribute(
+        CurrencyMapper.class.getName());
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    try {
+      handleSingleCurrencyRequest(req, resp);
+    } catch (CurrencyNotFoundException e) {
+      resp.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+    } catch (DatabaseException e) {
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+          e.getMessage());
+    } catch (Exception e) {
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+          DEFAULT_UNKNOWN_ERROR_MESSAGE);
     }
+  }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            String requestURI = req.getRequestURI();
-            if (requestURI.endsWith(CURRENCIES_PATH)) {
-                handleCurrencyListRequest(resp);
-            } else if (requestURI.endsWith(CURRENCIES_PATH + "/")) {
-                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Код валюты отсутствует в адресе");
-            } else {
-                handleSingleCurrencyRequest(req, resp);
-            }
-        } catch (Exception e) {
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Произошла ошибка, попробуйте выполнить запрос позднее");
-        }
+  private void handleSingleCurrencyRequest(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, CurrencyNotFoundException {
+    var code = (String) req.getAttribute(CODE);
+    var currency = currencyService.findByCode(code);
+    try (var writer = resp.getWriter()) {
+      resp.setStatus(HttpServletResponse.SC_OK);
+      writer.write(gson.toJson(currencyMapper.toDto(currency)));
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            var currencyDto = new CurrencyDto();
-            currencyDto.setCode(req.getParameter("code"));
-            currencyDto.setSign(req.getParameter("sign"));
-            currencyDto.setFullname(req.getParameter("name"));
-            var currency = currencyMapper.toModel(currencyDto);
-            var currencyWithId = currencyService.save(currency);
-            resp.sendRedirect(req.getRequestURI()+ "/" + currencyWithId.getCode());
-
-        } catch (Exception e) {
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  e.getMessage() + "Произошла ошибка, попробуйте выполнить запрос позднее");
-        }
-
-    }
-
-    private void handleCurrencyListRequest(HttpServletResponse resp) throws IOException {
-        try (var writer = resp.getWriter()) {
-            var currencies = currencyService.findAll();
-            var json = gson.toJson(currencyMapper.collectionToDto(currencies));
-            writer.write(json);
-            resp.setStatus(HttpServletResponse.SC_OK);
-        }
-    }
-
-    private void handleSingleCurrencyRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try (var writer = resp.getWriter()) {
-            String code = req.getRequestURI().substring(req.getRequestURI().lastIndexOf("/") + 1);
-            Optional<Currency> currencyOptional = currencyService.findByCode(code);
-            if (currencyOptional.isEmpty()) {
-                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Валюта с указанным кодом не найдена: " + code);
-                return;
-            }
-            var json = gson.toJson(currencyMapper.toDto(currencyOptional.get()));
-            writer.write(json);
-            resp.setStatus(HttpServletResponse.SC_OK);
-        }
-    }
+  }
 }
