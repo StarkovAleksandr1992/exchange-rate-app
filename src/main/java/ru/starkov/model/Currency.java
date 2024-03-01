ac2c32a45a0998597a7fdcd4651601c815981e8b
StarkovAleksandr <stanley59rus@gmail.com>
2024-03-01 16:28:53 +0500

    Refactor Currency class with builder pattern and full name attribute
    Updated the Currency class in the ru.starkov.model package to incorporate a builder pattern and a full name attribute.
    
    Changes include:
    
    Adding a builder pattern with the @Builder annotation to facilitate object construction with flexible initialization options.
    Renaming the 'fullname' attribute to 'fullName' for consistency with Java naming conventions.
    Updating class documentation to describe the purpose of the Currency model and the attributes it contains.
    These changes enhance the Currency class by providing a more structured and flexible approach to object creation, improving readability and maintainability in the codebase.

--- src/main/java/ru/starkov/model/Currency.java
+++ src/main/java/ru/starkov/model/Currency.java
@@ -1,11 +1,19 @@
 package ru.starkov.model;
 
+import lombok.Builder;
 import lombok.Data;
 
+/**
+ * This class represents a currency model. It contains the ID, code, full name and the sign of a
+ * currency.
+ */
 @Data
-public class Currency {
-    private Integer id;
-    private String code;
-    private String fullname;
-    private String sign;
+@Builder
+public final class Currency {
+
+  private Integer id;
+  private String code;
+  private String fullName;
+  private String sign;
+
 }
